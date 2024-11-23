44780307d320ce3c16b681c078745f860b2ef1f2
Spencer555 <46141953+Spencer555@users.noreply.github.com>
2024-11-23 14:54:01 +0000

    almost done

--- js/main.js
+++ js/main.js
@@ -1,55 +1,58 @@
-$(document).ready(function(){
 
-	//Navigation menu scrollTo 
-	$('header nav ul li a').click(function(event){
-		event.preventDefault();
-		var section = $(this).attr('href');
-		var section_pos = $(section).position();
-
-		if(section_pos){
-			$(window).scrollTo({top:section_pos.top, left:'0px'}, 1000);
-		}
-		
+document.addEventListener("DOMContentLoaded", function () {
+	// Navigation menu scrollTo
+	document.querySelectorAll("header nav ul li a").forEach(function (link) {
+		link.addEventListener("click", function (event) {
+			event.preventDefault();
+			const section = document.querySelector(this.getAttribute("href"));
+			if (section) {
+				window.scrollTo({
+					top: section.offsetTop,
+					left: 0,
+					behavior: "smooth",
+				});
+			}
+		});
 	});
 
-	$('.app_link').click(function(e){
-		event.preventDefault();
-		$(window).scrollTo({top:$("#hero").position().top, left:'0px'}, 1000);		
-	});
-	
-
-
-
-
-
-
-
-	//Show & Hide menu on mobile
-	$('.burger_icon').click(function(){
-		$('header nav').toggleClass('show');
-		$('header .burger_icon').toggleClass('active');
+	document.querySelectorAll(".app_link").forEach(function (appLink) {
+		appLink.addEventListener("click", function (event) {
+			event.preventDefault();
+			const hero = document.querySelector("#hero");
+			if (hero) {
+				window.scrollTo({
+					top: hero.offsetTop,
+					left: 0,
+					behavior: "smooth",
+				});
+			}
+		});
 	});
 
-	
+
+
+
+	// Show & Hide menu on mobile
+	const burgerIcon = document.querySelector(".burger_icon");
+	const nav = document.querySelector("header nav");
+
+	if (burgerIcon && nav) {
+		burgerIcon.addEventListener("click", function () {
+			nav.classList.toggle("show");
+			this.classList.toggle("active");
+		});
+	}
 
 
 
 
 
-
-	//wow.js on scroll animations initialization
-	wow = new WOW(
-	    {
-		  animateClass: 'animated',
-		  mobile: false,
-		  offset: 50
-		}
-	);
-	wow.init();
-
-
-
-
+	// wow.js on scroll animations initialization
+	new WOW({
+		animateClass: "animated",
+		mobile: false,
+		offset: 50,
+	}).init();
 
 
 
@@ -65,6 +68,7 @@ $(document).ready(function(){
 
 
 	//Nice scroll initialization
+	
 	$("html").niceScroll({
 		scrollspeed: 50,
 		autohidemode : false,
@@ -99,69 +103,77 @@ $(document).ready(function(){
 
 
 
+	// Mailchimp subscription form initialization
+	const submitForm = document.querySelector("#submit_form");
+	const mcSubmit = document.querySelector("#mc_submit");
+	const mcEmail = document.querySelector("#submit_form #mc-email");
+	const successMsg = document.querySelector("#success_msg");
+	const errorMsg = document.querySelector("#error_msg");
 
+	if (submitForm) {
+		submitForm.addEventListener("submit", function (event) {
+			event.preventDefault();
+			mcSubmit.disabled = true;
+			processing("icon", "loading");
 
-
-	//Mailchimp subscription form initialization
-	$('#submit_form').submit(function(){
-		$('#mc_submit').attr('disabled', 'disabled');		
-   		processing('icon', 'loading');
-	});
-
-	if($('#submit_form').length){
-		//Mailchim Subscription form
-		$('#submit_form').ajaxChimp({
-		    callback: chimpResponce
+			// Simulated AJAXChimp (use fetch/axios for real implementation)
+			setTimeout(() => {
+				const success = Math.random() > 0.5; // Simulated success/failure
+				if (success) {
+					processing("loading", "icon");
+					mcSubmit.disabled = false;
+					if (mcEmail) mcEmail.value = "";
+					if (successMsg) successMsg.style.display = "block";
+					if (errorMsg) errorMsg.style.display = "none";
+				} else {
+					processing("loading", "icon");
+					mcSubmit.disabled = false;
+					if (successMsg) successMsg.style.display = "none";
+					if (errorMsg) errorMsg.style.display = "block";
+				}
+			}, 1000); // Simulate response delay
 		});
-	}	
-
-	//Mail chimp callback function
-	function chimpResponce(resp) {
-   		if (resp.result === 'success') {   			
-   			processing('loading', 'icon');
-			$('#mc_submit').removeAttr('disabled', 'disabled');
-	        $('#submit_form #mc-email').val('');
-   			$('#error_msg').hide();
-   			$('#success_msg').show();
-	    }else{		
-   			processing('loading', 'icon');
-   			$('#success_msg').hide();
-   			$('#error_msg').show();
-	    	$('#mc_submit').removeAttr('disabled', 'disabled');
-	    }
-	}
-
-	function processing(hide, show){
-			$('#mc_submit i').removeClass(hide).addClass(show);
 	}
 
 
 
 
+	function processing(hide, show) {
+		const icon = mcSubmit.querySelector("i");
+		if (icon) {
+			icon.classList.remove(hide);
+			icon.classList.add(show);
+		}
+	}
 
+	// Popup video
+	const playVideo = document.querySelector("#play_video");
+	const closeVideo = document.querySelector(".close_video");
+	const aboutVideo = document.querySelector(".about_video");
 
-
-
-	//Popup video
-	$('#play_video').click(function(e){
-		e.preventDefault();	
-
-		var video_link = $(this).data('video');
-		video_link = '<iframe src="' + video_link + '" width="500" height="208" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-
-		$('.about_video').append(video_link).fadeIn(200);
-	});
-
-	$('.close_video').click(function(e){
-		e.preventDefault();	
-
-		$('.about_video').fadeOut(200,function(){
-			$('iframe', this).remove();
+	if (playVideo && aboutVideo) {
+		playVideo.addEventListener("click", function (event) {
+			event.preventDefault();
+			const videoLink = this.dataset.video;
+			if (videoLink) {
+				const iframe = document.createElement("iframe");
+				iframe.src = videoLink;
+				iframe.width = 500;
+				iframe.height = 208;
+				iframe.frameBorder = 0;
+				iframe.allowFullscreen = true;
+				aboutVideo.appendChild(iframe);
+				aboutVideo.style.display = "block";
+			}
 		});
+	}
 
-	});
-
-
-
+	if (closeVideo && aboutVideo) {
+		closeVideo.addEventListener("click", function (event) {
+			event.preventDefault();
+			aboutVideo.style.display = "none";
+			aboutVideo.innerHTML = ""; // Remove iframe
+		});
+	}
 
 });
